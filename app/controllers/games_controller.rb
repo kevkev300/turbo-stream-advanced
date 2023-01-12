d7143a8adbc64d52f709ccb4fc28efca0ac9b988
Kevin Liebholz <k.liebholz@portagon.com>
2023-01-12 16:17:42 +0100

    FEAT: add GamesController with new

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,4 @@
+class GamesController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'games#new'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :games, only: %i[]
 end
