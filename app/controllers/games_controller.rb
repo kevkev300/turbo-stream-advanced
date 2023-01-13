e8a6b511c2b097fea0ef062dd13da3386553862d
Kevin Liebholz <k.liebholz@portagon.com>
2023-01-13 15:30:07 +0100

    FEAT: create game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,15 @@
 class GamesController < ApplicationController
   def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new
+
+    if @game.save
+      redirect_to new_game_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'games#new'
 
-  resources :games, only: %i[]
+  resources :games, only: %i[new create]
 end
