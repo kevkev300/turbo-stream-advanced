a4476c7c8d1ff2d179459d7c5ceeb691310e6ad3
Kevin Liebholz <k.liebholz@portagon.com>
2023-01-13 16:21:33 +0100

    FEAT: need to player of that game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class GamesController < ApplicationController
+  def show
+    if player
+      'TODO'
+    else
+      flash[:alert] = 'You first need to sign up as a player for this game'
+      redirect_to new_game_player_path(game)
+    end
+  end
+
   def new
     @game = Game.new
   end
@@ -13,5 +22,15 @@ class GamesController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def game
+    @game ||= Game.find(params[:id])
+  end
+
+  def player
+    @player ||= game.players.find_by(id: params[:player_id])
   end
 end
