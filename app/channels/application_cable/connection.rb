9c75cedfb939649a3595aa802287bee070c1b12f
Kevin Liebholz <k.liebholz@portagon.com>
2023-01-17 11:38:07 +0100

    FEAT: reject bad player

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,8 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    def connect
+      player = Player.find_by(id: cookies['player_id'])
+      reject_unauthorized_connection unless player.character
+    end
   end
 end
